8f52c71354c88b6ad84e3efc46ed3a8064707bae
Roman Plyta <markian322@gmail.com>
2019-08-26 23:01:19 +0300

    Third Training Tasks

---
+++ Third_Training_Tasks/PersonClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using RandomListNameSpace;
+
+namespace Third_Training_Tasks
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            /*
+             * Перша частина завдання
+            Person person = new Person();
+
+            person.AddPerson(new Person() { Name = " Tom ", Age = 20, PhoneNumber = new List<string>() { "098 064", "094 456" } });
+            person.AddPerson(new Person() { Name = " Bill ", Age = 21, PhoneNumber = new List<string>() { "123 043", "038 093" } });
+            person.AddPerson(new Person() { Name = " Sam ", Age = 22, PhoneNumber = new List<string>() { "789 342", "458 932" } });
+
+            person.AddPersons(new Person() { Name = " John ", Age = 23, PhoneNumber = new List<string>() { "064 021", "012 984" } },
+                new Person() { Name = " Zack ", Age = 24, PhoneNumber = new List<string>() { "017 454", "034 888" } },
+                new Person() { Name = " Henry ", Age = 25, PhoneNumber = new List<string>() { "000 982", "983 127" } });
+
+            person.IntoduceAll();
+            */
+            // Друга частина завдання
+            RandomList rand = new RandomList();
+            rand.Runer();
+            rand.Introduce();
+        }
+
+       
+    }
+
+    class Person
+    {
+        List<Person> people = new List<Person>();
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public IEnumerable<string> PhoneNumber { get; set; }
+
+        public void IntoduceAll()
+        {
+            foreach (var p in people)
+            {
+                Console.WriteLine($"I'm {p.Name}, and I'm {p.Age} year old. My phone numbers is -> ");
+                foreach (var i in p.PhoneNumber)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public void AddPerson(Person person)
+        {
+            people.Add(person);
+        }
+
+        public void AddPersons(params Person[] persons)
+        {
+            people.AddRange(persons);
+        }
+    }
+}
---
+++ Third_Training_Tasks/RandomList.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomListNameSpace
+{
+    public class RandomList
+    {
+        public List<string> randomList = new List<string>();
+        char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * */
+        public void Runer()
+        {
+            AddRandomWords(randomList);
+            Console.WriteLine($" Elements count before distinct -> {GetCount(randomList)}");
+            SortList(randomList);
+            ReverseList(randomList);
+            DeleteDuplicates(randomList);
+            Console.WriteLine($" Elements count after distinct -> {GetCount(randomList)}");
+            DisplayPage(20);
+            Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+        }
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+        //Створення рандомних слів і одавання їх в список
+        public void AddRandomWords(List<string> tempList)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                addWord(randomWord(), tempList);
+            }
+        }
+
+
+        //Алгоритм обрахунку і виводу слів на певний сторінці
+        public void DisplayPage(int pageNumber)
+        {
+            Console.WriteLine($"Word on page -> {pageNumber}");
+            const int wordOnPage = 5;
+            int temp = pageNumber * wordOnPage - wordOnPage;
+            List<string> temporary = new List<string>();
+            for (int i = 0; i < wordOnPage; i++)
+                temporary.Add(randomList[temp + i]);
+
+            foreach (var i in temporary)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        //Метод для видалення дублікатів
+        public void DeleteDuplicates(List<string> tempList)
+        {
+            randomList = tempList.Distinct().ToList();
+        }
+
+        //Сортування списку
+        public void SortList(List<string> tempList)
+        {
+            tempList.Sort();
+        }
+
+        //Обернений список
+        public void ReverseList(List<string> tempList)
+        {
+            tempList.Reverse();
+        }
+
+        //Кількість елементів в списку
+        public int GetCount(List<string> tempList)
+        {
+            int count = tempList.Count;
+            return count;
+        }
+
+
+        //Метод розширення для додавання слів в список
+        public void addWord(string someWord, List<string> tempList)
+        {
+            tempList.Add(someWord);
+        }
+
+        //Метод для створення рандомних слів
+        public string randomWord()
+        {
+            var stringChars = new char[4];
+            Random random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            string finalString = new String(stringChars);
+            return finalString;
+        }
+
+        //Вивід всіх елеметів списку
+        public void Introduce()
+        {
+            Console.WriteLine("List of random words");
+            int count = 0;
+            foreach (var i in randomList)
+            {
+                Console.WriteLine(i);
+                count++;
+                if (count % 5 == 0)
+                {
+                    Console.WriteLine("-*-*-*-*-*-*-*-*-");
+                    count = 0;
+                }
+            }
+        }
+    }
+}
